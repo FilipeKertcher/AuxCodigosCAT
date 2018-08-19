75bf3b27cc02e262db84aa03adc57eb42a67aa0d
FilipeKertcher <filipekertcher97@gmail.com>
2018-08-19 18:34:38 -0300

    Create PartesAtingidas.sql

---
+++ PartesAtingidas.sql
@@ -0,0 +1,47 @@
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.30.50.000", "OUVIDO (externo, médio, interno, audição e equilíbrio)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.30.70.100", "OLHO (inclusive nervo ótico e visão)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.30.70.300", "NARIZ (inclusive fossas nasais, seios da face e olfato)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.30.70.500", "BOCA (Inclusive lábios, dentes, língua, garganta e paladar)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.30.70.700", "MANDÍBULA (inclusive queixo)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.30.70.800", "FACE, PARTES MÚLTIPLAS (qualquer combinação das partes acima)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.30.80.000", "CABEÇA, PARTES MÚLTIPLAS (qualquer combinação das partes acima)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.30.90.000", "CABEÇA, NIC") ;
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.40.00.000", "PESCOÇO") ;
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.35.10.000", "BRAÇO (entre o punho a o ombro)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.35.10.200", "BRAÇO (acima do cotovelo)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.50.10.400", "COTOVELO");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.50.10.600", "ANTEBRAÇO (entre o punho e o cotovelo)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.50.30.000", "PUNHO");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.50.50.000", "MÃO (exceto punho ou dedos)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.50.70.000", "DEDO");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.50.80.000", "MEMBROS SUPERIORES, PARTES MÚLTIPLAS (qualquer combinação das partes acima)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.50.90.000", "MEMBROS SUPERIORES, NIC");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.60.20.000", "OMBRO");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.60.30.000", "TÓRAX (inclusive órgãos internos)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.60.40.000", "DORSO (inclusive músculos dorsais, coluna e medula espinhal)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.60.50.000", "ABDOME (inclusive órgãos internos)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.60.60.000", "QUADRIS (inclusive pélvis, órgãos pélvicos e nádegas)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.60.70.000", "TRONCO, PARTE MÚLTIPLAS (qualquer combinação das partes acima)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.60.90.000", "TRONCO, NIC");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.70.10.000", "PERNA (entre o tornozelo e a pélvis)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.70.10.200", "COXA");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.70.10.400", "JOELHO");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.70.10.600", "PERNA (do tornozelo, exclusive, ao joelho, exclusive)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.70.30.000", "ARTICULAÇÃO DO TORNOZELO");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.70.50.000", "PÉ (exceto artelhos)");
+INSERT INTO CodigosPartesAtingidas (codigoParteId,codigoParteDescricao) VALUES ( "75.70.70.000", "ARTELHO");
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
